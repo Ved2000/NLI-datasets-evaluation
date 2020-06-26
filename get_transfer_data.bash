195f57b8b87b9ff41c7173da672f1e1421454ba5
Ved2000 <46571407+Ved2000@users.noreply.github.com>
2020-06-26 18:04:45 +0530

    Update get_transfer_data.bash

--- get_transfer_data.bash
+++ get_transfer_data.bash
@@ -29,28 +29,28 @@ PTBTOKENIZER="sed -f tokenizer.sed"
 mkdir $data_path
 
 
-SNLI='https://nlp.stanford.edu/projects/snli/snli_1.0.zip'
+
 MNLI='https://www.nyu.edu/projects/bowman/multinli/multinli_0.9.zip'
 
 
 
 ### download MNLI
 mkdir $data_path/MNLI
-curl -Lo $data_path/MNLI/multinli_1.0.zip $MNLI
-unzip $data_path/MNLI/multinli_1.0.zip -d $data_path/MNLI
-rm $data_path/MNLI/multinli_1.0.zip
+curl -Lo $data_path/MNLI/multinli_0.9.zip $MNLI
+unzip $data_path/MNLI/multinli_0.9.zip -d $data_path/MNLI
+rm $data_path/MNLI/multinli_0.9.zip
 rm -r $data_path/MNLI/__MACOSX
 
-for split in train dev test
+for split in train dev_matched dev_mismatched
 do
     fpath=$data_path/MNLI/$split.multinli.txt
-    awk '{ if ( $1 != "-" ) { print $0; } }' $data_path/MNLI/multinli_1.0/multinli_1.0_$split.txt | cut -f 1,6,7 | sed '1d' > $fpath
-    cut -f1 $fpath > $data_path/SNLI/labels.$split
+    awk '{ if ( $1 != "-" ) { print $0; } }' $data_path/MNLI/multinli_0.9/multinli_0.9_$split.txt | cut -f 1,6,7 | sed '1d' > $fpath
+    cut -f1 $fpath > $data_path/MNLI/labels.$split
     cut -f2 $fpath | $PTBTOKENIZER > $data_path/MNLI/s1.$split
     cut -f3 $fpath | $PTBTOKENIZER > $data_path/MNLI/s2.$split
     rm $fpath
 done
-rm -r $data_path/MNLI/multinli_1.0
+rm -r $data_path/MNLI/multinli_0.9
 
 
 

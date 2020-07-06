04ae67275b05dfa1fc3247a2689467c6076c225d
Ved2000 <46571407+Ved2000@users.noreply.github.com>
2020-07-06 15:45:49 +0530

    Update get_transfer_data.bash

--- get_transfer_data.bash
+++ get_transfer_data.bash
@@ -31,7 +31,7 @@ mkdir $data_path
 
 
 MNLI='https://www.nyu.edu/projects/bowman/multinli/multinli_0.9.zip'
-
+SNLI='https://nlp.stanford.edu/projects/snli/snli_1.0.zip'
 
 
 ### download MNLI
@@ -54,7 +54,23 @@ rm -r $data_path/MNLI/multinli_0.9
 
 
 
+### download SNLI
+mkdir $data_path/SNLI
+curl -Lo $data_path/SNLI/snli_1.0.zip $SNLI
+unzip $data_path/SNLI/snli_1.0.zip -d $data_path/SNLI
+rm $data_path/SNLI/snli_1.0.zip
+rm -r $data_path/SNLI/__MACOSX
 
+for split in train dev test
+do
+    fpath=$data_path/SNLI/$split.snli.txt
+    awk '{ if ( $1 != "-" ) { print $0; } }' $data_path/SNLI/snli_1.0/snli_1.0_$split.txt | cut -f 1,6,7 | sed '1d' > $fpath
+    cut -f1 $fpath > $data_path/SNLI/labels.$split
+    cut -f2 $fpath | $PTBTOKENIZER > $data_path/SNLI/s1.$split
+    cut -f3 $fpath | $PTBTOKENIZER > $data_path/SNLI/s2.$split
+    rm $fpath
+done
+rm -r $data_path/SNLI/snli_1.0
 
 
 
